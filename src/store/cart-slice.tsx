48511b3bb8d8c4517e7c688ca8677203fbaf0544
unknown <louis.alib19@gmail.com>
2026-02-20 23:49:27 +0100

    small update cart state

--- src/store/cart-slice.tsx
+++ src/store/cart-slice.tsx
@@ -1,26 +1,30 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface CartState {
-  items: number[];
+  items: {
+    id: number;
+    category: string;
+    categoryId: number;
+  }[]
 }
 
 const initialState: CartState = {
-  items: []
+  items: [],
 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    selectItem(state, action: PayloadAction<number>) {
-      const existingItem = state.items.find((item) => item === action.payload);
+    selectItem(state, action: PayloadAction<{ id: number; category: string; categoryId: number }>) {
+      const existingItem = state.items.find((item) => item.id === action.payload.id);
       if (existingItem) {
-        state.items = state.items.filter((item) => item !== action.payload);
+        state.items = state.items.filter((item) => item.id !== action.payload.id);
       } else {
         state.items.push(action.payload);
       }
     },
-    loadFavorites(state, action: PayloadAction<number[]>) {
+    loadFavorites(state, action: PayloadAction<{ id: number; category: string; categoryId: number }[]>) {
       state.items = action.payload;
     },
     clearCart(state) {
