2dbf026ec2b74290508039861766851fd46f49d9
unknown <louis.alib19@gmail.com>
2026-04-16 22:06:56 +0200

    minor changes

--- src/components/clothes/clothing-grid-skeleton.tsx
+++ src/components/clothes/clothing-grid-skeleton.tsx
@@ -1,13 +1,22 @@
+"use client"
+
+import { useParams } from "next/navigation"
+
 export default function ClothingGridSkeleton() {
+    const params = useParams()
     return (
-        <ul className="grid grid-cols-[repeat(auto-fit,minmax(250px,250px))] gap-x-4 gap-y-2 p-5 mx-auto justify-center">
-            {[...Array(12)].map((_, i) => (
-                <li key={i} className="animate-pulse">
-                    <div className="bg-gray-200 aspect-3/4 rounded-lg mb-2"></div>
-                    <div className="bg-gray-200 h-4 w-3/4 rounded mb-2"></div>
-                    <div className="bg-gray-200 h-4 w-1/2 rounded"></div>
-                </li>
-            ))}
-        </ul>
+        <>
+            <p className="text-5xl font-bold m-4 ml-5 text-center md:text-start md:ml-50 capitalize text-background">{params.garmentType}</p>
+            <ul className="grid grid-cols-[repeat(auto-fit,minmax(250px,250px))] gap-x-4 gap-y-2 p-5 mx-auto justify-center">
+                {[...Array(12)].map((_, i) => (
+
+                    <li key={i} className="animate-pulse">
+                        <div className="bg-gray-200 aspect-3/4 rounded-lg mb-2"></div>
+                        <div className="bg-gray-200 h-4 w-3/4 rounded mb-2"></div>
+                        <div className="bg-gray-200 h-4 w-1/2 rounded"></div>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
